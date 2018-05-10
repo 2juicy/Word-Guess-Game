d151e50e9f672ac8f3a04e2e783fc4259c6c4cb5
2Juicy <j0hnnywchiang@gmail.com>
2018-05-09 20:14:51 -0700

    style and started on game

---
+++ javascript/game.js
@@ -0,0 +1,61 @@
+//Variable list
+var lives = 12;
+var wins = 0;
+var loses = 0;
+var alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+var words = ['doomfist', 'genji', 'tracer', 'hanzo', 'pharah', 'reaper', 'sombra', 'mercy', 'zenyatta', 'offense', 'defense', 'support']
+var answer = '';
+var guesses = [''];
+var letters = [''];
+var correctLetters = [''];
+//Generates a random word then splits into single letters.
+function randomWord(){
+let randomWord = (Math.floor(Math.random() * words.length));
+answer = words[randomWord];
+letters = answer.split('');
+}
+randomWord();
+//Display current word with _ before game starts.
+var hidden = answer.replace(/[a-z]+/gi, '_ ');
+document.getElementById("currentWord").innerHTML = hidden;
+console.log(hidden);
+
+
+//On key event method.
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    //if same letter is chosen
+    for (let i = 0; i < guesses.length; i++) {
+        if (guesses[i] === userGuess) {
+            return; //kills the function.
+        }}
+
+    //if guess matches a letter in word
+    if (letters.forEach === userGuess){
+        
+    }
+    //checks if input is part of alphabet, will only run if input is valid.
+    for (var i = 0; i < alphabet.length; i++) {   
+        if (alphabet[i] === userGuess) {
+        //if letter does not match   
+        if (letters.forEach !== userGuess){
+            lives--;
+        }
+        //pushes already used letters into an array to track lives, reset game when out of lives.
+        guesses.push(userGuess)
+        if (guesses.length === 13){
+            loses++;
+            lives = 12;
+            guesses = [''];
+            randomWord();
+        }
+    }}
+console.log(userGuess);
+console.log(answer);
+console.log(letters);
+
+document.getElementById("yourGuesses").innerHTML = "Letters already guessed: " + guesses.join(' ');
+document.getElementById("yourLoses").innerHTML = "Loses: " + loses;
+document.getElementById("yourWins").innerHTML = "Wins: " + wins;
+document.getElementById("yourLives").innerHTML = "Number of guesses remaining: " + lives;
+}   
